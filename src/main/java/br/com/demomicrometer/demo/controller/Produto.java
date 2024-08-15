b9e0498d32be3178a82881d6298241f24e5da675
flaviohenso <flavio.henso@gmail.com>
2024-08-15 05:30:00 -0300

    Criando metrica personlizada

--- src/main/java/br/com/demomicrometer/demo/controller/Produto.java
+++ src/main/java/br/com/demomicrometer/demo/controller/Produto.java
@@ -1,11 +1,48 @@
 package br.com.demomicrometer.demo.controller;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Produto {
+    
+    @JsonProperty("id")
+    private Long id;
+    
+    @JsonProperty("nome")
+    private String nome;
+    
+    @JsonProperty("preco")
+    private double preco;
 
-
-
-    public Produto(long l, String string, double d) {
-        System.out.println("Produto [id=" + l + ", nome=" + string + ", preco=" + d + "]");
+    // Construtor público
+    public Produto(Long id, String nome, double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
     }
 
+    // Getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    // Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
 }
--- src/main/java/br/com/demomicrometer/demo/controller/ProdutoController.java
+++ src/main/java/br/com/demomicrometer/demo/controller/ProdutoController.java
@@ -7,19 +7,46 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
+
 @Controller
 public class ProdutoController {
+
+    private final MeterRegistry meterRegistry;
+    private final Counter myCounter;
+    private final Timer myTimer;
+
+    public ProdutoController(MeterRegistry meterRegistry) {
+        this.meterRegistry = meterRegistry;
+        this.myCounter = Counter.builder("minha.metrica.personalizada")
+                .description("Contador de chamadas ao método processar")
+                .tag("metodo", "processar")
+                .register(meterRegistry);
+        this.myTimer = Timer.builder("minha.metrica.timer")
+                .description("Tempo de processamento do método processar")
+                .tag("metodo", "processar")
+                .register(meterRegistry);
+    }
 
     /*
      * Endpoint para listar todos os produtos mockados
      */
     @GetMapping("/produtos")
     public ResponseEntity<List<Produto>> listarProdutos() {
+        myCounter.increment();
+        Timer.Sample sample = Timer.start(meterRegistry);
+        //iniciando metrica de timer
+        
         List<Produto> produtos = new ArrayList<>();
         produtos.add(new Produto(1L, "Produto 1", 10.0));
         produtos.add(new Produto(2L, "Produto 2", 20.0));
         produtos.add(new Produto(3L, "Produto 3", 30.0));
+        produtos.add(new Produto(1L, "Produto 1", 10.0));
+        produtos.add(new Produto(2L, "Produto 2", 20.0));
+        produtos.add(new Produto(3L, "Produto 3", 30.0));
+        sample.stop(myTimer);
         return ResponseEntity.ok(produtos);
     }
-
 }
